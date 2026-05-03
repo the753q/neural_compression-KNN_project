b1add52a301af4eadf532a1d60ad7d61fe7d0969
Tomas Macak <50169298+the753q@users.noreply.github.com>
2026-05-03 16:34:13 +0200

    minor edits

--- src/train.py
+++ src/train.py
@@ -1,23 +1,22 @@
+import os
 import lightning.pytorch as pl
 import torch
 from lightning.pytorch.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers import CSVLogger
-import os
+from lightning.pytorch.loggers import CSVLogger
 
 from data import ClassImagesDataModule, DF2KDataModule
-
 from models import get_model
 
-from evaluate import eval_compression, eval_patches
 
 torch.set_float32_matmul_precision("medium")
 
 datamodule_default_imagenet10k = ClassImagesDataModule(
     data_dir="datasets/imagenet_10K/imagenet_subtrain",
-    batch_size=16,
+    batch_size=64,
+    num_workers=10,
     random_crop=True,
     ycbcr=True,
-    patch_size=128
+    patch_size=128,
 )
 
 datamodule_df2k = DF2KDataModule(
@@ -25,18 +24,19 @@ datamodule_df2k = DF2KDataModule(
     test_dir="datasets/DF2K/test",
     batch_size=8,
     ycbcr=True,
-    random_crop=True
+    random_crop=True,
 )
+
 
 def experiment1():
     """
-        Train a basic AE on ImageNet.
+    Train a basic AE on ImageNet.
     """
     EXPERIMENT_NAME = "basic_imagenet10k"
     MODEL_NAME = "basic"
     EPOCHS = 15
     LEARNING_RATE = 2e-4
-    
+
     model = get_model(MODEL_NAME, learning_rate=LEARNING_RATE)
 
     checkpoint_filename = f"{EXPERIMENT_NAME}-{MODEL_NAME}-best"
@@ -52,18 +52,21 @@ def experiment1():
     trainer = pl.Trainer(
         max_epochs=EPOCHS,
         accelerator="auto",
-        callbacks=[checkpoint_callback]
+        precision="bf16-mixed",
+        callbacks=[checkpoint_callback],
     )
 
-    print("="*30)
+    print("=" * 30)
     print(f"Started experiment: {EXPERIMENT_NAME}")
 
     print(f"Starting training for {MODEL_NAME}...")
     trainer.fit(model, datamodule_default_imagenet10k)
-    print(f"Training complete. Best model saved to checkpoints/{os.path.basename(checkpoint_filename)}.ckpt")
+    print(
+        f"Training complete. Best model saved to checkpoints/{os.path.basename(checkpoint_filename)}.ckpt"
+    )
 
     print(f"Finished experiment: {EXPERIMENT_NAME}")
-    print("="*30)
+    print("=" * 30)
 
     # compute latents for quantization
     print("Computing priors...")
@@ -79,22 +82,25 @@ def experiment1():
     all_latents = torch.cat(all_latents, dim=0)
 
     # load best model weights
-    best_model = ( model.__class__).load_from_checkpoint(checkpoint_callback.best_model_path)
+    best_model = (model.__class__).load_from_checkpoint(
+        checkpoint_callback.best_model_path
+    )
     best_model.to(device)
     # compute priors from latents
     best_model.compute_priors(all_latents)
     # save model as torch object
     torch.save(best_model, f"checkpoints/manual/{MODEL_NAME}_best.pt")
 
+
 def experiment2():
     """
-        Train a basic DCAL 2018 on ImageNet..
+    Train a basic DCAL 2018 on ImageNet..
     """
     EXPERIMENT_NAME = "dcal_df2k"
     MODEL_NAME = "DCAL_2018"
-    EPOCHS = 20 
+    EPOCHS = 20
     LEARNING_RATE = 1e-4
-    
+
     model = get_model(MODEL_NAME, learning_rate=LEARNING_RATE)
 
     checkpoint_filename = f"{EXPERIMENT_NAME}-{MODEL_NAME}-best"
@@ -104,7 +110,7 @@ def experiment2():
         filename=checkpoint_filename,
         save_top_k=1,
         monitor="val_loss",
-        mode="min"
+        mode="min",
     )
 
     csv_logger = CSVLogger("logs/", name=EXPERIMENT_NAME)
@@ -116,24 +122,29 @@ def experiment2():
         logger=csv_logger,
     )
 
-    print("="*30)
+    print("=" * 30)
     print(f"Started experiment: {EXPERIMENT_NAME}")
 
     print(f"Starting training for {MODEL_NAME}...")
     trainer.fit(model, datamodule_default_imagenet10k)
-    print(f"Training complete. Best model saved to checkpoints/{os.path.basename(checkpoint_filename)}")
+    print(
+        f"Training complete. Best model saved to checkpoints/{os.path.basename(checkpoint_filename)}"
+    )
 
     print(f"Finished experiment: {EXPERIMENT_NAME}")
-    print("="*30)
+    print("=" * 30)
 
     # load best model weights
-    best_model = ( model.__class__).load_from_checkpoint(checkpoint_callback.best_model_path)
+    best_model = (model.__class__).load_from_checkpoint(
+        checkpoint_callback.best_model_path
+    )
     # save model as torch object
     torch.save(best_model, f"checkpoints/manual/{MODEL_NAME}_best.pt")
 
+
 def main():
-   # pass
-    #experiment1()
+    # pass
+    # experiment1()
     experiment2()
 
 
